bd637f4bd288a9ada240fa4bde954b35916cf495
Chris Durden <cdurden@gmail.com>
2021-09-09 20:55:29 -0500

    Adding binary oauth

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 exports.OAuth = require("./lib/oauth").OAuth;
 exports.OAuthEcho = require("./lib/oauth").OAuthEcho;
+exports.OAuthBinary = require("./lib/oauth-binary").OAuthBinary;
 exports.OAuth2 = require("./lib/oauth2").OAuth2;
